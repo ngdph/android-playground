7fc10b7cf340501c03641dc2b2680239d1928457
AnhKhoa2174 <tkhoa4040@gmail.com>
2024-04-25 16:14:09 +0700

    Add MyApplication_1

--- khoa.trananh/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ khoa.trananh/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
@@ -16,14 +17,17 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
-//import androidx.compose.material.Text
-
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                AndroidAliens()
+                AndroidAliensColumns()
+                AndroidAliensRows()
             }
         }
     }
@@ -44,16 +48,43 @@ fun AndroidAlien(
 }
 
 @Composable
-fun AndroidAliens() {
-    Column {
+fun AndroidAliensColumns() {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
         AndroidAlien(
             color = Color.Red,
             modifier = Modifier
                 .size(100.dp)
                 .padding(4.dp)
         )
+        Spacer(Modifier.size(16.dp))
         AndroidAlien(
             color = Color.Black,
+            modifier = Modifier
+                .size(100.dp)
+                .padding(4.dp)
+        )
+    }
+}
+@Composable
+fun AndroidAliensRows() {
+    Row (
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.Top
+    ){
+        AndroidAlien(
+            color = Color.Green,
+            modifier = Modifier
+                .size(100.dp)
+                .padding(4.dp)
+        )
+        Spacer(Modifier.size(16.dp))
+        AndroidAlien(
+            color = Color.Yellow,
             modifier = Modifier
                 .size(100.dp)
                 .padding(4.dp)
@@ -65,6 +96,7 @@ fun AndroidAliens() {
 @Composable
 fun PreviewAndroidAliens() {
     MyApplicationTheme {
-        AndroidAliens()
+        AndroidAliensColumns()
+        AndroidAliensRows()
     }
 }
