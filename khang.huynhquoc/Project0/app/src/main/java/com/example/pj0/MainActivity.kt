c010fd8e6fe274e91eabd5e0f38e20e3ac6f2c20
Khang <khangnha7@gmail.com>
2024-05-02 17:57:07 +0700

    practice jetpack compose

--- khang.huynhquoc/Project0/app/src/main/java/com/example/pj0/MainActivity.kt
+++ khang.huynhquoc/Project0/app/src/main/java/com/example/pj0/MainActivity.kt
@@ -1,47 +1,66 @@
 package com.example.pj0
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.pj0.ui.theme.Pj0Theme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Pj0Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
+            Greeting("Khang")
+            GreetingText("Hello World","hi")
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name:String) {
+    Column(modifier = Modifier.padding(start = 120.dp)) {
+        Spacer(modifier = Modifier.height(400.dp))
+        Text(" Happy\n Birthday \n $name!",
+            style = TextStyle(
+                fontSize = 50.sp, // Thay đổi kích thước văn bản ở đây
+                lineHeight = 100.sp,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                //textAlign = TextAlign.Center
+            )
+        )
+    }
+}
+
+@Composable
+fun GreetingText(message: String,from:String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = message,
+        fontSize = 100.sp,
+        lineHeight = 116.sp,
+    )
+    Text(
+        text=from,
+        fontSize = 100.sp ,
+        lineHeight = 200.sp,
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Pj0Theme {
-        Greeting("Android")
-    }
-}
+
+
+
+
